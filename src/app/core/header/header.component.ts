2c5a049814cf0e42eb240fdf5cb0085a0c68cb3e
nadirashvilininutsa <160115012+nadirashvilininutsa@users.noreply.github.com>
2024-02-19 17:05:35 +0400

    Header: - Created header design - Implemented responsiveness of header design - Created routing for header elements - Updated common styles:   1. Set box-sizing for all elements to border-box   2. Set margin for all elements to 0 - Updated HTML styles:   1. Set font-size to 62.5%   2. Implemented responsive sizes - Created enum with the list of product categories - Created common service and implemented opened category variable

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductCategories } from 'src/app/models/api-models';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private router: Router, private commonService: CommonService) {}
 
+  productCategories = ProductCategories;
+
+  navigateToProducts(product: ProductCategories) {
+    this.commonService.openedCategory = product;
+    this.router.navigate(['main/categories']);
+  }
 }
