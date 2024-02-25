1aef507b8b3794ada8afb5ccc684b84e47064b2e
nadirashvilininutsa <160115012+nadirashvilininutsa@users.noreply.github.com>
2024-02-26 00:48:41 +0400

    6 - Categories - Add URLs of Articles` and Categories` images to db.json file accordingly - Complete articles component - Create responsive design for articles component - Complete categories component - Create responsive design for categories component - Render articles and categories in the panel component - Modify api-models.ts file accordingly

--- src/app/main/article/article.component.ts
+++ src/app/main/article/article.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from 'src/app/models/api-models';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent {
+  constructor(private commonService: CommonService) {}
 
+  articles$: Observable<Article[]> | undefined;
+
+  getArticles(): Observable<Article[]> {
+    return this.commonService.getArticles();
+  }
+
+  ngOnInit() {
+    this.articles$ = this.getArticles();
+  }
 }
--- src/app/main/panel/panel.component.ts
+++ src/app/main/panel/panel.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Commercial } from 'src/app/models/api-models';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.scss']
+  styleUrls: ['./panel.component.scss'],
 })
 export class PanelComponent {
+  constructor(private commonService: CommonService) {}
 
+  commercials$: Observable<Commercial> | undefined;
+
+  getCommercials(): Observable<Commercial> {
+    return this.commonService.getCommercials();
+  }
+
+  ngOnInit() {
+    this.commercials$ = this.getCommercials();
+  }
 }
