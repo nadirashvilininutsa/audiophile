8b055846a77e7504730062c823d688a00489cece
nadirashvilininutsa <160115012+nadirashvilininutsa@users.noreply.github.com>
2024-02-27 16:01:39 +0400

    7 - Categories - Correct keys in db.json file: 1. Instead of "new product", use "newProduct" 2. Instead of "in the box", use "inTheBox" - Correct routing link parameters in footer and header. Instead of hard copy texts, use dynamic text as parameters that you get from back - Correct routing in main-routing.module.ts file: path: "category/:product" should redirect to ProductComponent - Add routing to categories.component.html file - Remove unnecessary code from main.component.html file - Make interfaces in api-models.ts file more accurate: common information of the product should be under ProductBasics interface and Product interface should extend it - Make corresponding changes to all files that use ProductBasics interface - Write code for product rendering in product component - Make responsive design for the product component

--- src/app/main/product/product.component.ts
+++ src/app/main/product/product.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription, filter, map, switchMap, tap } from 'rxjs';
+import {
+  Categories,
+  Product,
+  ProductCategories,
+} from 'src/app/models/api-models';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private commonService: CommonService
+  ) {}
 
+  private paramsSubscription: Subscription | undefined;
+  productCategory: ProductCategories | undefined;
+  models$: Observable<Product[]> | undefined;
+
+  getModels(): Observable<Product[]> {
+    return this.commonService.getProducts().pipe(
+      switchMap((categories) =>
+        categories.filter(
+          (category) => category.product === this.productCategory
+        )
+      ),
+      map((category) => category.models)
+    );
+  }
+
+  ngOnInit() {
+    this.paramsSubscription = this.activatedRoute.params.subscribe((params) => {
+      this.productCategory = params['product'];
+      this.models$ = this.getModels();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.paramsSubscription) {
+      this.paramsSubscription.unsubscribe();
+    }
+  }
 }
